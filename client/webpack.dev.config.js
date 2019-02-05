df8831c1d8b28aa861264f3a7bf75b8495822c81
crocoM4 <portapozze@libero.it>
2019-02-05 23:00:33 +0100

    Update packages
    Update packages

--- client/webpack.dev.config.js
+++ client/webpack.dev.config.js
@@ -1,47 +1,47 @@
 /* eslint import/no-extraneous-dependencies: 0 */
-const autoprefixer = require('autoprefixer');
-const path = require('path');
-const webpack = require('webpack');
+const autoprefixer = require("autoprefixer");
+const path = require("path");
+const webpack = require("webpack");
 
 module.exports = {
-  mode: 'development',
+  mode: "development",
   watch: true,
-  entry: ['@babel/polyfill', './src/index.jsx'],
+  entry: ["@babel/polyfill", "./src/index.jsx"],
   output: {
-    publicPath: 'client/public/dist/',
-    filename: '[name].js',
-    path: path.resolve(__dirname, 'public/dist'),
+    publicPath: "client/public/dist/",
+    filename: "[name].js",
+    path: path.resolve(__dirname, "public/dist")
   },
-  devtool: 'inline-source-map',
+  devtool: "inline-source-map",
   resolve: {
-    extensions: ['.js', '.jsx'],
+    extensions: [".js", ".jsx"]
   },
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
-        loaders: ['babel-loader'],
         exclude: /node_modules/,
+        loaders: ["babel-loader"]
       },
       {
         test: /\.(sass|css)$/,
         use: [
-          { loader: 'style-loader' },
-          { loader: 'css-loader' },
+          { loader: "style-loader" },
+          { loader: "css-loader" },
           {
-            loader: 'postcss-loader',
+            loader: "postcss-loader",
             options: {
-              plugins: () => [autoprefixer({ browsers: ['> 1%', 'IE >= 10'] })],
-            },
+              plugins: () => [autoprefixer({ browsers: ["> 1%", "IE >= 10"] })]
+            }
           },
-          { loader: 'sass-loader' },
-        ],
-      },
-    ],
+          { loader: "sass-loader" }
+        ]
+      }
+    ]
   },
   plugins: [
     new webpack.NamedModulesPlugin(),
     new webpack.NoEmitOnErrorsPlugin(),
-    new webpack.IgnorePlugin(/\.svg$/),
-  ],
+    new webpack.IgnorePlugin(/\.svg$/)
+  ]
 };
--- client/webpack.prod.config.js
+++ client/webpack.prod.config.js
@@ -1,76 +1,76 @@
 /* eslint import/no-extraneous-dependencies: 0 */
-const autoprefixer = require('autoprefixer');
-const path = require('path');
-const webpack = require('webpack');
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const autoprefixer = require("autoprefixer");
+const path = require("path");
+const webpack = require("webpack");
+const UglifyJSPlugin = require("uglifyjs-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 
-const packageFile = require('./package.json');
+const packageFile = require("./package.json");
 
 module.exports = {
-  mode: 'production',
+  mode: "production",
   entry: {
-    main: './src/index.jsx',
-    vendor: Object.keys(packageFile.dependencies),
+    main: "./src/index.jsx",
+    vendor: Object.keys(packageFile.dependencies)
   },
   output: {
-    publicPath: 'client/public/dist/',
-    filename: '[name].js',
-    chunkFilename: '[name].js',
-    path: path.resolve(__dirname, 'public/dist'),
+    publicPath: "client/public/dist/",
+    filename: "[name].js",
+    chunkFilename: "[name].js",
+    path: path.resolve(__dirname, "public/dist")
   },
   resolve: {
-    extensions: ['.js', '.jsx'],
+    extensions: [".js", ".jsx"]
   },
   optimization: {
     splitChunks: {
       cacheGroups: {
         commons: {
           test: /[\\/]node_modules[\\/]/,
-          name: 'vendor',
-          chunks: 'all',
-        },
-      },
+          name: "vendor",
+          chunks: "all"
+        }
+      }
     },
     minimizer: [
       new UglifyJSPlugin({
-        parallel: true,
+        parallel: true
       }),
-      new OptimizeCSSAssetsPlugin({}),
-    ],
+      new OptimizeCSSAssetsPlugin({})
+    ]
   },
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
-        loaders: ['babel-loader'],
-        exclude: /node_modules/,
+        loaders: ["babel-loader"],
+        exclude: /node_modules/
       },
       {
         test: /\.(sass|css)$/,
         use: [
           MiniCssExtractPlugin.loader,
-          { loader: 'css-loader' },
+          { loader: "css-loader" },
           {
-            loader: 'postcss-loader',
+            loader: "postcss-loader",
             options: {
-              plugins: () => [autoprefixer({ browsers: ['> 1%', 'IE >= 10'] })],
-            },
+              plugins: () => [autoprefixer({ browsers: ["> 1%", "IE >= 10"] })]
+            }
           },
-          { loader: 'sass-loader' },
-        ],
-      },
-    ],
+          { loader: "sass-loader" }
+        ]
+      }
+    ]
   },
   plugins: [
     new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify('production'),
+      "process.env.NODE_ENV": JSON.stringify("production")
     }),
     new webpack.HashedModuleIdsPlugin(),
     new webpack.IgnorePlugin(/\.svg$/),
     new MiniCssExtractPlugin({
-      filename: '[name].css',
-    }),
-  ],
+      filename: "[name].css"
+    })
+  ]
 };
