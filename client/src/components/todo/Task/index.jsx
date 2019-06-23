6b333dfefaaba59c97fb86742dd3f42821f9ea0a
crocoM4 <portapozze@libero.it>
2019-06-23 11:46:09 +0200

    Clean up
    Clean up

--- client/src/components/todo/Task/index.jsx
+++ client/src/components/todo/Task/index.jsx
@@ -3,18 +3,41 @@ import React from "react";
 import EditableTaskComponent from "./editable";
 import StaticTaskComponent from "./static";
 
-import type { StaticProps } from "./static";
-import type { EditableProps } from "./editable";
+import type { Task } from "../../../models/task";
+import type { Category } from "../../../models/category";
 
 type Props = {
-  ...StaticProps,
-  ...EditableProps,
+  // ...StaticProps,
+  +onUndo?: () => void,
+  +onCreate?: ({}) => void,
+  // ...EditableProps,
+  +index?: number,
+  +onDelete?: () => void,
+  +onComplete?: () => void,
+  +onCategoryClick?: Category => void,
+  +onSetCategory?: Category => void,
+  +onCreateCategory?: string => void,
+  +onRemoveCategory?: Category => void,
+  // Common
+  +task: Task,
   +creating?: boolean
 };
+
 type State = {};
 
 class TaskComponent extends React.PureComponent<Props, State> {
   static defaultProps = {
+    onUndo: () => {},
+    onCreate: () => {},
+
+    index: 0,
+    onCategoryClick: () => {},
+    onSetCategory: () => {},
+    onCreateCategory: () => {},
+    onRemoveCategory: () => {},
+
+    onDelete: undefined,
+    onComplete: undefined,
     creating: false
   };
 
